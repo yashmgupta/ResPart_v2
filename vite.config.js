9aaf2ccd3c178e18f376e7316088ffc78e1a8c02
yashmgupta <72779706+yashmgupta@users.noreply.github.com>
2025-12-25 11:29:12 +0700

    Update base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/research-swipe-react/", // IMPORTANT for GitHub Pages
+  base: "/ResPart_v2/", // REQUIRED for this repo
 });
